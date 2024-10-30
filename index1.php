a7f283ec1830c438b023be70eafe9ff1ad379a56
KoraRiko <62887693+KoraRiko@users.noreply.github.com>
2024-10-30 13:04:49 +0200

    Update index1.php

--- index1.php
+++ index1.php
@@ -3,18 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Footer Example</title>
+    <title>Time in footer</title>
 </head>
 <body>
-    <!-- Content of your webpage -->
-
     <footer>
         <?php 
-      
         function displayDateTime() {
-            return date("Y-m-d H:i:s"); // Returns the current date and time in 'YYYY-MM-DD HH:MM:SS' format
+            return date("Y-m-d H:i:s");
         }
-
         echo displayDateTime(); 
         ?>
     </footer>
